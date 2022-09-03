80d4c0a993a0bf61b6138b2a679432bc13149117
Snikkertjuh <snikker.zakelijk@protonmail.com>
2022-09-03 17:24:10 +0200

    Update

--- app.py
+++ app.py
@@ -6,8 +6,4 @@ app = flask.Flask(__name__)
 def main():
     return flask.render_template("home.html")
 
-@app.get("/nick")
-def nick():
-    return "Wie is nick?"
-
 app.run(debug=True, host="0.0.0.0", port=4002)
